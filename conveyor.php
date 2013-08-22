16b9331ee6065a1e23bcea06179ee76732338cfd
Simon Miles <simon@bigspring.co.uk>
2013-08-22 17:40:31 +0100

    Added plugin header so WP recognises it as a plugin

---
+++ conveyor.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: Conveyor
+Plugin URI: http://badgersaregreat.com
+Description: A simple carousel slider plugin for WordPress built with Twitter Bootstrap and Flexslider by Woothemes.
+Version: 1.0
+Author: BigSpring "the home of ping"
+Author URI: http://www.bigspring.co.uk
+License: GPL2
+*/
